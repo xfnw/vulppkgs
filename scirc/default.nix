2ef85a08f6ddf884859a9b725ec40454acbbec0e
xfnw <xfnw@riseup.net>
2022-04-12 18:31:55 -0400

    scirc: 1.2 -> 1.3

--- scirc/default.nix
+++ scirc/default.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   pname = "scirc";
-  version = "1.2";
+  version = "1.3";
 
   src = fetchFromGitHub {
     owner = "xfnw";
     repo = "scirc";
     rev = "v${version}";
-    sha256 = "sha256-bSWxVNSyAN5grBfaSG7hGUnD67LCdY7ru+arF8nRJWU=";
+    sha256 = "sha256-xdY1m/zTEKiIO1SFKgxsuCtELSagevmbT6TgdRkLJmA=";
   };
 
   installPhase = ''
