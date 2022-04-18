2b1a65e32bf8c3d4c0abe26a61d9ca950bfaa8bb
xfnw <xfnw@riseup.net>
2022-04-17 23:14:01 -0400

    scirc: 1.3 -> 1.3.1
    adds manual page

--- scirc/default.nix
+++ scirc/default.nix
@@ -4,16 +4,17 @@
 
 stdenv.mkDerivation rec {
   pname = "scirc";
-  version = "1.3";
+  version = "1.3.1";
 
   src = fetchFromGitHub {
     owner = "xfnw";
     repo = "scirc";
     rev = "v${version}";
-    sha256 = "sha256-xdY1m/zTEKiIO1SFKgxsuCtELSagevmbT6TgdRkLJmA=";
+    sha256 = "sha256-i+qRs4xWDZjJCsrZGUDqwu3OJc4QuPpE9SEE3EZNRPo=";
   };
 
   installPhase = ''
     install -D scirc $out/bin/scirc
+    install -D scirc.1 $out/man/man1/scirc.1
   '';
 }
