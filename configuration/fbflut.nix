39eea225826e58636f6c50068e772d4b564fb241
xfnw <xfnw@riseup.net>
2022-12-27 16:53:52 -0500

    format

--- configuration/fbflut.nix
+++ configuration/fbflut.nix
@@ -5,26 +5,28 @@
 
 with pkgs;
 
-let fbflut =
-  stdenv.mkDerivation rec {
-    pname = "fbflut";
-    version = "unstable-2022-03-14";
+let
+  fbflut =
+    stdenv.mkDerivation rec {
+      pname = "fbflut";
+      version = "unstable-2022-03-14";
 
-    src = fetchFromGitHub {
-      owner = "xfnw";
-      repo = "fbflut";
-      rev = "02554ea5472cc92b1dd30c0fadb960af0e804cda";
-      sha256 = "sha256-sX/DgeA9HXDswnENaeVcuF2tIDlXv1ddNQhR9iR5Bkk=";
+      src = fetchFromGitHub {
+        owner = "xfnw";
+        repo = "fbflut";
+        rev = "02554ea5472cc92b1dd30c0fadb960af0e804cda";
+        sha256 = "sha256-sX/DgeA9HXDswnENaeVcuF2tIDlXv1ddNQhR9iR5Bkk=";
+      };
+
+      postConfigure = ''
+        substituteInPlace Makefile --replace c99 $CC
+      '';
+
+      installPhase = ''
+        install -D fbflut $out/bin/fbflut
+      '';
     };
-
-    postConfigure = ''
-      substituteInPlace Makefile --replace c99 $CC
-    '';
-
-    installPhase = ''
-      install -D fbflut $out/bin/fbflut
-    '';
-  }; in
+in
 {
   users.users.fbflut = {
     isSystemUser = true;
