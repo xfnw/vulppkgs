ca3803dd0ba123da8801ca43550a9800e606c61d
xfnw <xfnw@riseup.net>
2023-06-02 18:33:15 -0400

    scirc: 1.6.0 -> 1.7.0

--- scirc/default.nix
+++ scirc/default.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   pname = "scirc";
-  version = "1.6.0";
+  version = "1.7.0";
 
   src = fetchFromGitHub {
     owner = "xfnw";
     repo = "scirc";
     rev = "v${version}";
-    sha256 = "sha256-UMZPn3oAjxyhG510L8C05RvmZGmx1EybEwbDi1/phiw=";
+    sha256 = "sha256-gGvbY1qp9EyBYdiopna4XJkl4nQsSQ1xyZuwr7j4LKE=";
   };
 
   installPhase = ''
