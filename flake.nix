3bcf8595636b4c15c336372c04fbb9436e641387
xfnw <xfnw@riseup.net>
2024-08-22 10:03:45 -0400

    flake: expose module

--- flake.nix
+++ flake.nix
@@ -7,5 +7,7 @@
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = (import nixpkgs) { inherit system; };
-      in { packages = (import ./. { inherit pkgs; }); });
+      in { packages = (import ./.) { inherit pkgs; }; }) // {
+        nixosModule = (import ./modules);
+      };
 }
