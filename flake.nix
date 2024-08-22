daea02ca1ae750052a3b22cb7e88422d2063fa1c
xfnw <xfnw@riseup.net>
2024-08-21 20:37:07 -0400

    flake: init

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = (import nixpkgs) { inherit system; };
+      in { packages = (import ./. { inherit pkgs; }); });
+}
