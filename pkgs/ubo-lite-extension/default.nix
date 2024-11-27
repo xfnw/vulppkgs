89a7dde78efdd2d8f96d28fd7230d09fcbdd0f02
xfnw <xfnw@riseup.net>
2024-11-27 08:53:07 -0500

    ubo-lite-extension: 2024.11.20.858 -> 2024.11.25.1376

--- pkgs/ubo-lite-extension/default.nix
+++ pkgs/ubo-lite-extension/default.nix
@@ -7,15 +7,15 @@ let
   crx = fetchurl {
     # returned from:
     # https://clients2.google.com/service/update2/crx?acceptformat=crx2,crx3&prodversion=${chromiumMajorVersion}.0&x=id%3Dddkjiahejlhfcafbddmgiahcphecmpfh%26installsource%3Dondemand%26uc
-    url = "https://clients2.googleusercontent.com/crx/blobs/AW50ZFv_31Vc2YisToBJYcugR6Itni96T9g3sOeDgtFtdbPb5Ruj57NWyg0GK0kvTNGFYARskBGW-RbgyOGecoNk8M83GHBzLIkORd6w9fEg07X7JhpgLTKCdEgiqq8qA_nDAMZSmuVRMBHZVTNadvUWHfP5DhYxQO9Y2w/DDKJIAHEJLHFCAFBDDMGIAHCPHECMPFH_2024_11_20_858.crx";
-    hash = "sha256-QxIaXXHN1ztS4VUWJMB0KD46Vmk0BkidzJLAvpR6CS0=";
+    url = "https://clients2.googleusercontent.com/crx/blobs/AW50ZFuHmhhzmbZMdTjUtNgYGA91Q6EunDfSGqkJ8BuYZy5y-mvCaqHb2ykv-ISjkXBad_rqbEOtJu15MwQ9kBhU4ZXfOKDw19JvFfqYLfve3lNFQvBGUK9H-pUcQgmcsYtwAMZSmuUB7SSBReQCyOsKfArTI2b5dSTlsg/DDKJIAHEJLHFCAFBDDMGIAHCPHECMPFH_2024_11_25_1376.crx";
+    hash = "sha256-fi/atHHXw5cHiWy4kKjstXYbTogPC5sQZYNtFAMjKcw=";
   };
 in
 writeTextFile {
   name = "ubo-lite-extension";
   text = builtins.toJSON {
     external_crx = crx;
-    external_version = "2024.11.20.858";
+    external_version = "2024.11.25.1376";
   };
   destination = "/share/chromium/extensions/${extid}.json";
 }
