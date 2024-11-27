4262a922e7eb7f18577392bd949d5a3b261f1618
xfnw <xfnw@riseup.net>
2024-11-26 20:17:17 -0500

    pbcli: 2.4.0 -> 2.5.1

--- pkgs/pbcli/default.nix
+++ pkgs/pbcli/default.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "pbcli";
-  version = "2.4.0";
+  version = "2.5.1";
 
   src = fetchFromGitHub {
     owner = "Mydayyy";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-Y6R6Z2EBxnzhPs/belXH8t39tqwXE/brl3gMUItWjfQ=";
+    hash = "sha256-41Sgey8xDyZk23WG+Yj+uvI5qVCvY/SbbyBTJGYdmaA=";
   };
 
-  cargoHash = "sha256-F0TNm4SsW8bb1A5nySXUbn7/RGyBE6PBMCekBq/Ubg8=";
+  cargoHash = "sha256-5URF33zY84d8FautDosKzg3BdoMbWqEGIHNgy1ZQ/y0=";
 
   nativeBuildInputs = [ pkg-config ];
 
