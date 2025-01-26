37479c734e4db5ed478f21d50940fb5522d04d56
xfnw <xfnw@riseup.net>
2025-01-26 15:30:08 -0500

    scripts: avoid re-downloading up to date extensions

--- scripts/update-extension.sh
+++ scripts/update-extension.sh
@@ -5,9 +5,16 @@ set -eux
 
 package="$1"
 extid="$(sed -n 's/^ *extid = "\([^"]*\)";$/\1/p' "$package")"
+oldurl="$(sed -n 's/^ *url = "\([^"]*\)";$/\1/p' "$package")"
 chrmajor="$(nix-instantiate --eval --xml -E "with import <nixpkgs>{}; lib.versions.major ungoogled-chromium.version" | xml sel -t -v /expr/string/@value)"
 gupdate="$(curl "https://clients2.google.com/service/update2/crx?acceptformat=crx2,crx3&prodversion=$chrmajor.0&x=id%3D$extid%26installsource%3Dondemand%26uc")"
 url="$(xml sel -N res=http://www.google.com/update2/response -t -m //res:updatecheck -v @codebase <<<"$gupdate")"
+
+if [ "$oldurl" = "$url" ]; then
+	echo already up to date
+	exit
+fi
+
 version="$(xml sel -N res=http://www.google.com/update2/response -t -m //res:updatecheck -v @version <<<"$gupdate")"
 sha256="$(nix-prefetch-url --type sha256 "$url")"
 sri="$(nix-hash --to-sri --type sha256 "$sha256")"
