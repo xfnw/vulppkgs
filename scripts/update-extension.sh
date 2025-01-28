b78a09b578a7b6967533a5b2f3893ee555e52263
xfnw <xfnw@riseup.net>
2025-01-27 22:11:46 -0500

    scripts: check version instead of url
    apparently google does not make the url constant for each version

--- scripts/update-extension.sh
+++ scripts/update-extension.sh
@@ -5,17 +5,17 @@ set -eux
 
 package="$1"
 extid="$(sed -n 's/^ *extid = "\([^"]*\)";$/\1/p' "$package")"
-oldurl="$(sed -n 's/^ *url = "\([^"]*\)";$/\1/p' "$package")"
+oldver="$(sed -n 's/^ *external_version = "\([^"]*\)";$/\1/p' "$package")"
 chrmajor="$(nix-instantiate --eval --xml -E "with import <nixpkgs>{}; lib.versions.major ungoogled-chromium.version" | xml sel -t -v /expr/string/@value)"
 gupdate="$(curl "https://clients2.google.com/service/update2/crx?acceptformat=crx2,crx3&prodversion=$chrmajor.0&x=id%3D$extid%26installsource%3Dondemand%26uc")"
-url="$(xml sel -N res=http://www.google.com/update2/response -t -m //res:updatecheck -v @codebase <<<"$gupdate")"
+version="$(xml sel -N res=http://www.google.com/update2/response -t -m //res:updatecheck -v @version <<<"$gupdate")"
 
-if [ "$oldurl" = "$url" ]; then
+if [ "$oldver" = "$version" ]; then
 	echo already up to date
 	exit
 fi
 
-version="$(xml sel -N res=http://www.google.com/update2/response -t -m //res:updatecheck -v @version <<<"$gupdate")"
+url="$(xml sel -N res=http://www.google.com/update2/response -t -m //res:updatecheck -v @codebase <<<"$gupdate")"
 sha256="$(nix-prefetch-url --type sha256 "$url")"
 sri="$(nix-hash --to-sri --type sha256 "$sha256")"
 
