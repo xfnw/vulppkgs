6c7e8f1065ed01ad949894201d06f3018c076dde
xfnw <xfnw@riseup.net>
2025-06-07 17:58:44 -0400

    dotbin: 0-unstable-2025-02-04 -> 0-unstable-2025-06-07

--- pkgs/dotbin/default.nix
+++ pkgs/dotbin/default.nix
@@ -7,13 +7,13 @@
 
 stdenvNoCC.mkDerivation {
   pname = "dotbin";
-  version = "0-unstable-2025-02-04";
+  version = "0-unstable-2025-06-07";
 
   src = fetchFromGitHub {
     owner = "xfnw";
     repo = "dotfiles";
-    rev = "fb265dd7006612254517d5ad812489396d8553d0";
-    hash = "sha256-sVGUhBfS7oLzIk2bvzjLfrNqh2QBFVzR9CMXEgq1P5Y=";
+    rev = "f9509aaebe4b447c196dc1350299db081dd8bb6d";
+    hash = "sha256-tKtpVefORwIpDxXuCwAOCyG7B1awe35Ke5g4BkIpLP4=";
   };
 
   buildInputs = [ python3 perl ];
