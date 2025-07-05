3fc5179c717fd4575c4694d37967d47bef3e078f
xfnw <xfnw@riseup.net>
2025-07-04 21:59:35 -0400

    fuzzball: add findnext patch

--- pkgs/fuzzball/default.nix
+++ pkgs/fuzzball/default.nix
@@ -17,6 +17,13 @@ stdenv.mkDerivation rec {
   };
 
   patches = [
+    # https://github.com/fuzzball-muck/fuzzball/pull/735
+    (fetchpatch {
+      name = "tanabi-fix-findnext.patch";
+      url = "https://github.com/fuzzball-muck/fuzzball/commit/4d99fdb07b2d213db0ea0829b598f0d2086466f7.patch";
+      hash = "sha256-rPVjcG1W5C6v2WBBCXwcAGFeExKqAU8Cb2zSx1njmjk=";
+    })
+
     # https://github.com/fuzzball-muck/fuzzball/pull/753
     (fetchpatch {
       name = "pointsdragon-fix-crash.patch";
