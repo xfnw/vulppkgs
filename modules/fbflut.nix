c3764f1cab22c656544da7dcae94c30637c2921d
xfnw <xfnw@riseup.net>
2026-06-13 15:39:01 -0400

    fbflut: run in a systemd service directly
    fbflut was previously being run in getty since that starts it at the
    right time and takes control of the tty so that output goes in the right
    place. however, doing it that way makes logind give fbflut a session,
    and switch-to-configuration-ng unconditionally assumes that every user
    (even system users) with a logind session will have dbus stuff running,
    which makes an annoying (but as far as i can tell harmless?) error every
    activation
    
    instead, run it in a systemd service that sends StandardOutput to the
    tty. this allows it to still send the control characters that turn off
    cursor blinking to the right place whilst not having a login session to
    confuse switch-to-configuration-ng

--- modules/fbflut.nix
+++ modules/fbflut.nix
@@ -18,13 +18,30 @@ in
     users.users.fbflut = {
       isSystemUser = true;
       group = "video";
-      shell = "${cfg.package}/bin/fbflut";
     };
 
-    systemd.services."getty@tty1".overrideStrategy = "asDropin";
-    systemd.services."getty@tty1".serviceConfig.ExecStart = [
-      ""
-      "@${pkgs.util-linux}/sbin/agetty agetty --login-program ${gcfg.loginProgram} --autologin fbflut --noclear --keep-baud %I 115200,38400,9600 $TERM"
-    ];
+    systemd.services.fbflut = {
+      enable = true;
+      after = [ "getty.target" ];
+      conflicts = [ "getty@tty1.service" "autovt@tty1.service" ];
+      wantedBy = [ "multi-user.target" ];
+
+      # FIXME: surely there is a better way to wait for /dev/fb0?
+      # adding a dependency on dev-fb0.device just times out :(
+      preStart = ''
+        while [ ! -e /dev/fb0 ]; do
+          ${pkgs.coreutils}/bin/sleep 1
+        done
+      '';
+
+      serviceConfig = {
+        ExecStart = "${cfg.package}/bin/fbflut";
+        StandardOutput = "tty";
+        TTYPath = "/dev/tty1";
+      };
+    };
+
+    systemd.services."getty@tty1".enable = false;
+    systemd.services."autovt@tty1".enable = false;
   };
 }
