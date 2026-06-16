2a43728b9e614bcaeb0c800ddfb1b9fc4d023ba4
xfnw <xfnw@riseup.net>
2026-06-16 18:20:58 -0400

    metrics-relay: 0-unstable-2026-05-16 -> 0-unstable-2026-06-16

--- pkgs/metrics-relay/default.nix
+++ pkgs/metrics-relay/default.nix
@@ -2,18 +2,18 @@
 , rustPlatform
 }:
 
-rustPlatform.buildRustPackage (finalAttrs: {
+rustPlatform.buildRustPackage {
   pname = "metrics-relay";
-  version = "0-unstable-2026-05-16";
+  version = "0-unstable-2026-06-16";
 
   src = fetchFromGitHub {
     owner = "xfnw";
     repo = "workspace";
-    rev = "165a4b868fc5873189fb3fa0b1d89abc2f301bfe";
-    hash = "sha256-EdIqOEaOaf71cNQuvvM5bzQZ0+60EL/Bb8oWU45q/qo=";
+    rev = "d19204ddad8882d0c5b61dd725584ac06c319371";
+    hash = "sha256-kvziZ2fLCaaY344cA8zftV64vN9fC1CxEGJJNkFqtCQ=";
   };
 
-  cargoHash = "sha256-dBl7a0WpXc7/mOMut6KdoSJuycCavnGWJ/vP7qPi7Jk=";
+  cargoHash = "sha256-WqLDY0PQ3hrloYIbhFfrkKIun198gvSH8xoV16rDjHw=";
 
   buildAndTestSubdir = "crates/metrics-relay";
-})
+}
