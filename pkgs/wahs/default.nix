8bacce83d7549c18b8b71fe608b131d9e39cda8f
xfnw <xfnw@riseup.net>
2026-07-05 14:32:25 -0400

    wahs: 0-unstable-2026-07-03 -> 0-unstable-2026-07-05

--- pkgs/wahs/default.nix
+++ pkgs/wahs/default.nix
@@ -4,13 +4,13 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "wahs";
-  version = "0-unstable-2026-07-03";
+  version = "0-unstable-2026-07-05";
 
   src = fetchFromGitHub {
     owner = "xfnw";
     repo = "wahs";
-    rev = "78a87f316c6d2ed34a2bfe066446bfc3f4f09fdd";
-    hash = "sha256-SHPLKiPuhzrBta0flaXsvQCOHIpif5uApX6AB470uOE=";
+    rev = "25d7bf8dee47a70efd1e0b8298f3b466a8131e4c";
+    hash = "sha256-DJ+k+HYqXu44ifGn65TtW3Puc/sqMwoORqk5kZzHjVA=";
   };
 
   cargoHash = "sha256-ndchlJ9e8RPdNIsCxwbMX+3rncQLV89SVh2nFeACrB0=";
